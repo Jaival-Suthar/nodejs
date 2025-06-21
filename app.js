27ad7dae7105a46ea24f8d70df240cae6b2efd1a
Jaival <jaivalsuthar21@gmail.com>
2025-06-21 20:28:48 +0530

    Basic globalThis understanding

---
+++ app.js
@@ -0,0 +1,10 @@
+var name1 = "Learning Node.js";
+
+var a = 10;
+var b = 20;
+console.log(name1);
+console.log(a + b);
+console.log(global);
+//console.log(this); //Empty object in Node.js
+//console.log(globalThis); //Global object in Node.js
+//console.log(globalThis === global); //true
