27c0d3d29524e93df92ab0b8aa81c9b864fb7e92
Jaival <jaivalsuthar21@gmail.com>
2025-06-24 00:07:04 +0530

    feat: learnt about CommonJS modules using module.exports and require

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+require('./xyz.js'); // Importing xyz.js file  one module to another
+require('./sum.js'); // Importing sum.js file       .js is optional in Node.js
+
+// const obj = require('./sum.js'); OLD WAY          // Importing the function from sum.js
+
+// Destructuring on the Fly
+ const { calculateSum, x } = require('./sum.js'); // Importing specific exports
+ calculateSum(10, 20); // Calling the function directly
+ console.log(x); // Accessing the exported variable directly
 var name1 = "Learning Node.js";
 
 var a = 10;
 var b = 20;
 console.log(name1);
 console.log(a + b);
-console.log(global);
+
+// obj.calculateSum(a, b); // Calling the function from sum.js
+// console.log(obj.x); // Accessing the exported variable from sum.js
+// console.log(global);
 //console.log(this); //Empty object in Node.js
 //console.log(globalThis); //Global object in Node.js
 //console.log(globalThis === global); //true
+
